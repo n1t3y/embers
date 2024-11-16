3e206cf9e6b28342f606e13de820789bdbb19d16
n1t3 <n1t3@tuta.io>
2024-11-16 17:12:26 +0300

    add platform

---
+++ embers/src/platform.cpp
@@ -0,0 +1,7 @@
+#include "platform.hpp"
+
+namespace embers {
+
+Platform::Error Platform::last_error_ = Platform::Error::kUnknown;
+
+}
---
+++ embers/src/platform.hpp
@@ -0,0 +1,97 @@
+#pragma once
+#include <embers/config.hpp>
+#include <embers/defines.hpp>
+#include <embers/logger.hpp>
+
+#include "error_code.hpp"
+#include "window.hpp"
+
+namespace embers {
+
+class Platform {
+ public:
+  enum class Error : ErrorType {
+    kOk      = (ErrorType)embers::Error::kOk,
+    kUnknown = (ErrorType)embers::Error::kUnknown,
+    // propagate from window::Window
+    kInitGLFW = (ErrorType)embers::Error::kWindowInitGLFW,
+    // propagate from window::Window
+    kCreateWindow = (ErrorType)embers::Error::kWindowCreateWindow,
+  };
+
+ private:
+  static Error last_error_;
+  Window       window_;
+
+ public:
+  Platform() = delete;
+  inline Platform(const config::Platform &config);
+  Platform(const Platform &platform) = delete;
+  constexpr Platform(Platform &&platform);
+  inline ~Platform();
+
+  constexpr explicit                operator bool() const;
+  Platform                         &operator=(const Platform &rhs) = delete;
+  constexpr Platform               &operator=(Platform &&rhs);
+  constexpr bool                    operator==(const Platform &rhs) const;
+  constexpr bool                    operator!=(const Platform &rhs) const;
+  EMBERS_ALWAYS_INLINE static Error get_last_error();
+};
+
+}  // namespace embers
+
+// implementation
+
+namespace embers {
+
+inline Platform::Platform(const config::Platform &config)
+    : window_(
+          config.resolution.width,
+          config.resolution.height,
+          config.application_name
+      ) {
+  if (!(bool)window_) {
+    auto err = to_error_code(Window::get_last_error());
+    EMBERS_ERROR(
+        "Unable to init embers platform: {}; Zombie platform is going to be "
+        "initialized",
+        err
+    );
+  }
+
+  EMBERS_INFO("Platform initialized");
+  return;
+}
+
+constexpr Platform::Platform(Platform &&platform)
+    : window_(std::move(platform.window_)) {}
+
+constexpr Platform::operator bool() const {
+  return (bool)window_ &&  //
+         true;
+};
+
+inline Platform::~Platform() {
+  EMBERS_INFO("Platform terminated");
+  return;
+}
+
+constexpr Platform &Platform::operator=(Platform &&rhs) {
+  window_ = std::move(rhs.window_);
+  return *this;
+};
+
+constexpr bool Platform::operator==(const Platform &rhs) const {
+  return window_ == rhs.window_ &&  //
+         true;
+}
+
+constexpr bool Platform::operator!=(const Platform &rhs) const {
+  return !operator==(rhs);
+}
+
+EMBERS_ALWAYS_INLINE Platform::Error Platform::get_last_error() {
+  return last_error_;
+}
+
+}  // namespace embers
