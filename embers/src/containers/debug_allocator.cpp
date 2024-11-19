4eb7d0d22f156fd31059b2798356a157a729003d
n1t3 <n1t3@tuta.io>
2024-11-19 19:12:47 +0300

    fix build on mscv

--- embers/src/containers/debug_allocator.cpp
+++ embers/src/containers/debug_allocator.cpp
@@ -4,8 +4,8 @@
 
 namespace embers::containers {
 
-static DebugAllocatorInfo debug_allocator_info
-    [(int)DebugAllocatorTags::kMax - (int)DebugAllocatorTags::kMin + 1]{};
+DebugAllocatorInfo debug_allocator_info
+    [(int)DebugAllocatorTags::kMax - (int)DebugAllocatorTags::kMin + 1] = {};
 
 }  // namespace embers::containers
 
